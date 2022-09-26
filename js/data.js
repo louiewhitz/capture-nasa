05d7b7e60a26d417c743704c96115e53221237b4
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-26 15:50:23 +0000

    finished html and get requests for feature 1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,19 @@
 /* exported data */
+// eslint-disable-next-line no-unused-vars
+var nasa = {
+  view: 'start-page',
+  favorites: [],
+  results: [],
+  nextEntryId: 1
+};
+
+/* var previousEntryJSON = localStorage.getItem('data');
+if (previousEntryJSON !== null) {
+  data = JSON.parse(previousEntryJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
+function beforeUnload() {
+  var jsonData = JSON.stringify(data);
+  localStorage.setItem('data', jsonData);
+} */
